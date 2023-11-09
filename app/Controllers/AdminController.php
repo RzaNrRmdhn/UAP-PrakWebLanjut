1d944012de9f196a0b6e88ecddbb04094717e648
SiskaHermayanti <siskahermayanti25@gmail.com>
2023-11-09 23:14:26 +0700

    dashboard admin

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -10,4 +10,20 @@ class AdminController extends BaseController
     {
         //
     }
+    public function dashboard(){
+        return view('admin/home/dashboard');
+    }
+    public function list_admin(){
+        return view('admin/home/list_admin');
+    }
+    public function list_seller(){
+        return view('admin/home/list_seller');
+    }
+    public function list_customer(){
+        return view('admin/home/list_customer');
+    }
+    public function admin_profile(){
+        return view('admin/home/admin_profile');
+    }
+  
 }
